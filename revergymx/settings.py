940c843971acc372c873d55a6a8d660e208d3639
SamuelsgTesting <126424860+SamuelsgTesting@users.noreply.github.com>
2023-03-09 20:07:04 -0600

    Update settings.py

--- revergymx/settings.py
+++ revergymx/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-7ppocbnx@w71dcuinn*t^_mzal(t@o01v3fee27g%rg18fc5d@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 AUTH_USER_MODEL = "rh.User"
 SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
